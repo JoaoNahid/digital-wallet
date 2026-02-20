7467c0240b4c216e97b46ae2cab8877dc9f1c08e
João Nahid <joaonahiddev@gmail.com>
2026-02-20 15:01:41 -0300

    :sparkles: Feature: add labels

--- app/Enums/TransactionStatus.php
+++ app/Enums/TransactionStatus.php
@@ -7,4 +7,14 @@ enum TransactionStatus: string {
     case Completed = 'completed';
     case Failed = 'failed';
     case Reversed = 'reversed';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Pending => 'Pendente',
+            self::Completed => 'Completo',
+            self::Failed => 'Falhou',
+            self::Reversed => 'Estornado',
+        };
+    }
 }
--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -7,4 +7,13 @@ enum TransactionType: string {
     case TransferOut = 'transfer_out';
     case TransferIn = 'transfer_in';
     case Reversal = 'reversal';
+
+    public function label(): string {
+        return match ($this) {
+            self::Deposit => 'Depósito',
+            self::TransferOut => 'Transferência Enviada',
+            self::TransferIn => 'Transferência Recebida',
+            self::Reversal => 'Estorno',
+        };
+    }
 }
