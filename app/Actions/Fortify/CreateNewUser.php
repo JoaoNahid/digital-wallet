02553fcb275a96e7dbe8a0b132a0456eab0b2782
João Nahid <joaonahiddev@gmail.com>
2026-02-22 13:11:45 -0300

    :hammer: Refactor: call the action CreateWalletAction when creates a new user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Actions\Wallet\CreateWalletAction;
 use App\Concerns\PasswordValidationRules;
 use App\Concerns\ProfileValidationRules;
 use App\Models\User;
@@ -12,26 +13,30 @@ class CreateNewUser implements CreatesNewUsers
 {
     use PasswordValidationRules, ProfileValidationRules;
 
+    public function __construct(
+        private readonly CreateWalletAction $m_CreateWalletAction,
+    ) { }
+
     /**
      * Validate and create a newly registered user.
      *
      * @param  array<string, string>  $input
      */
-    public function create(array $input): User
+    public function create(array $p_Input): User
     {
-        Validator::make($input, [
+        Validator::make($p_Input, [
             ...$this->profileRules(),
             'password' => $this->passwordRules(),
         ])->validate();
 
         $v_User = User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => $input['password'],
+            'name' => $p_Input['name'],
+            'email' => $p_Input['email'],
+            'password' => $p_Input['password'],
         ]);
 
         if ($v_User) {
-            $v_User->wallet()->create();
+            $this->m_CreateWalletAction->execute($v_User);
         }
 
         return $v_User;
