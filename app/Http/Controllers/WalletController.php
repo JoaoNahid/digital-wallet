54952571857e7d15f28bbad2221dba83c65c58aa
João Nahid <joaonahiddev@gmail.com>
2026-02-23 16:10:44 -0300

    :hammer: Refactor: send transactions to the wallet view

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -9,6 +9,7 @@ use App\DTOs\Wallet\TransferDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Wallet\DepositRequest;
 use App\Http\Requests\Wallet\TransferRequest;
+use App\Http\Resources\TransactionResource;
 use App\Http\Resources\WalletResource;
 use App\Services\Wallet\TransactionService;
 use App\Services\Wallet\WalletService;
@@ -21,13 +22,19 @@ class WalletController extends Controller
 {
     public function __construct(
         private readonly WalletService $m_WalletService,
+        private readonly TransactionService $m_TransactionService,
     ) {}
 
     public function index(Request $p_Request): Response {
         $v_Wallet = $this->m_WalletService->getWallet($p_Request->user());
+        $v_Transactions = $this->m_TransactionService->getPaginatedTransactions(
+            $v_Wallet->id,
+            5
+        );
 
         return Inertia::render('wallet/Index', [
-            'wallet' => WalletResource::make($v_Wallet)->toArray($p_Request)
+            'wallet' => WalletResource::make($v_Wallet)->toArray($p_Request),
+            'transactions' => TransactionResource::collection($v_Transactions)
         ]);
     }
 
